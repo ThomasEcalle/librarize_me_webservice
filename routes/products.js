92ae97fd2d2c319d4008d654f20f310bd5538186
Jean <spyroo913@hotmail.com>
2017-06-05 19:11:13 +0200

    products delete/search (left : search from amazon)

--- routes/products.js
+++ routes/products.js
@@ -9,47 +9,43 @@ const User = models.User;
 const Product = models.Product;
 const Friendship = models.Friendship;
 const client = amazon.createClient({
-  awsId: "AKIAINQMPU52EXNVKOMA",
-  awsSecret: "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",
+  awsId: "",
+  awsSecret: "",
   awsTag: "node02-20"
-});
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Welcome in products part');
 });
 
 /******************************************
 *             Create product              * -------- PROBLEM WITH PRICE BECAUSE DOUBLE AND MULTIPLE CURRENCIES POSSIBLE !!<!!>!!
 *******************************************/
 
-router.post("/create/:barcode", function(req, res, next){
+router.post("/create/:code/:codetype", function(req, res, next){
     if(req.user){
         client.itemLookup({
-          idType: 'UPC',
-          itemId: req.params.barcode
+          idType: req.params.codetype,
+          itemId: req.params.code
         }).catch(function(err) {
-            res.status(400).json({
-                result: 0,
-                message: "This product ID don't exist, try another."
-            });
+            res.json(err);
         }).then(function(results){
             let attributes = results[0].ItemAttributes[0];
-            res.json(results);
-            switch(attributes.Binding.join(';')){
+            switch(attributes.Binding.join(';')){ 
+                // all possibilities behind
                 case "Blu-ray":
                 case "DVD":
-                    getJSONForDVD(attributes, req, function(){
+                case "Audio CD":
+                case "CD":
+                case "Hardcover":    
+                case "Paperback":
+                    getJSON(attributes, req, function(){
                         res.status(200).json({
                             result: 1,
                             message: "The product has been well added to the database."
                         });
+                    }, function(){
+                        res.status(400).json({
+                            result: 0,
+                            message: "Parsing impossible from amazon result to database."
+                        })
                     });
-                case "CD":
-                    // TO DO
-                    break;
-                case "BOOK":
-                    // TO DO
                     break;
                 default: 
                     res.status(400).json({
@@ -61,24 +57,33 @@ router.post("/create/:barcode", function(req, res, next){
     }
 });
 
-function getJSONForDVD(attributes, req, next){
-   models.Product.create({
-        type: attributes.Binding.join(';'),
-        bar_code: req.params.barcode,
-        date: attributes.ReleaseDate.join(';'),
-        price: attributes.ListPrice[0].FormattedPrice.join(';'),
-        name: attributes.Title.join(';'),
-        editor: attributes.Publisher.join(';'),
-        genre: null,
-        autor: attributes.Director.join(';'),
-        actors: attributes.Actor.join(';'),
+function getJSON(attributes, req, next, error){
+    let type = attributes.Binding ? attributes.Binding.join(';') : null;
+    let date = attributes.ReleaseDate ? attributes.ReleaseDate.join(';') : 
+        (attributes.PublicationDate ? attributes.PublicationDate.join(';') : null); 
+    let price = attributes.ListPrice ?
+        (attributes.ListPrice[0].FormattedPrice ? attributes.ListPrice[0].FormattedPrice.join(';') : null) : null;
+    let name = attributes.Title ? attributes.Title.join(';') : null;
+    let editor = attributes.Publisher ? attributes.Publisher.join(';') : 
+        (attributes.Label ? attributes.Label.join(';') : null);
+    let genre = null;
+    let actors = attributes.Actor ? attributes.Actor.join(';') : null;
+    let autor = attributes.Author ? attributes.Author.join(';') : 
+        (attributes.Artist ? attributes.Artist.join(';') : 
+        (attributes.Director ? attributes.Director.join(';') : null));
+    
+    models.Product.create({
+        type: type,
+        bar_code: req.params.codetype + ';' + req.params.code,
+        date: date,
+        price: price,
+        name: name,
+        editor: editor,
+        genre: genre,
+        autor: autor,
+        actors: actors,
         user_id: req.user.id
-   }).catch(function(err) {
-       res.status(400).json({
-            result: 0,
-            message: "Parsing impossible from amazon result to daabase."
-        });
-   }).then(next());
+    }).catch(error).then(next);
 }
 
 /******************************************
@@ -87,21 +92,115 @@ function getJSONForDVD(attributes, req, next){
 
 router.put("/delete/:product_id", function(req, res, next){
     if(req.user){
-        models.Product.destroy({where: {id: 1}}).then(function(arg){
-           res.json("coucou"); 
+        Product.destroy({
+            where:
+            {
+                user_id: req.user.id,
+                id: req.params.product_id
+            }
+        }).then(function(resp){
+            if(resp == 1)
+                res.status(200).json({
+                    result: 1,
+                    message: "The product has been well deleted"
+                });
+            else if(resp == 0)
+                res.status(400).json({
+                    result: 0,
+                    message: "The product hasn't been deleted, maybe the id does not exists or the product doesn't belong to the active user."
+                });
+            res.json(resp);
         }).catch(next);
     }
 });
 
 /******************************************
-*             Get my products             *
+*             Get all products            *
 *******************************************/
+
+router.get("/search/", function(req, res, next){
+    if(req.user){
+        Product.findAll({
+          where:
+          {
+            deleted_at: null
+          }
+        }).then(function(products){
+          let result = [];
+          for (let i in products){
+            result.push({ id: products[i].id, bar_code: products[i].bar_code, user_id: products[i].user_id });
+          }
+          res.status(200).send(result);
+        }).catch(next);
+    }
+
+})
 
 /******************************************
 *             Get a product               *
 *******************************************/
 
+router.get("/search/:id", function(req, res, next){
+    if(req.user){
+        Product.findOne({
+          where:
+          {
+            id: req.params.id
+          }
+        }).then(function(product){
+            if(product) res.status(200).send(product);
+            res.status(400).json({
+                result: 0,
+                message: "ID not found."
+            });
+        }).catch(next);
+    }  
+})
+
+/******************************************
+*             Get my products             *
+*******************************************/
+
+router.get("/search/user/active", function(req, res, next){
+    if(req.user){
+        Product.findAll({
+          where:
+          {
+            user_id: req.user.id,
+            deleted_at: null
+          }
+        }).then(function(products){
+          let result = [];
+          for (let i in products){
+            result.push({ id: products[i].id, bar_code: products[i].bar_code, user_id: products[i].user_id });
+          }
+          res.status(200).send(result);
+        }).catch(next);
+    }
+
+})
+
 /******************************************
 *             Get products for user       *
 *******************************************/
+
+router.get("/search/user/:user_id", function(req, res, next){
+    if(req.user){
+        Product.findAll({
+          where:
+          {
+            user_id: req.params.user_id,
+            deleted_at: null
+          }
+        }).then(function(products){
+          let result = [];
+          for (let i in products){
+            result.push({ id: products[i].id, bar_code: products[i].bar_code, user_id: products[i].user_id });
+          }
+          res.status(200).send(result);
+        }).catch(next);
+    }
+
+})
+
 module.exports = router;
